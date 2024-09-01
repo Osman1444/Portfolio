3ce6b72e373ed619593b7336404e4e62d15ebd9a
Osman1444 <osmanminecraft76@gmail.com>
2024-09-02 00:45:44 +0300

    New version

--- main.js
+++
@@ -1,8 +0,0 @@
-const skillsContainer = document.getElementById('Scontainer'); // Get the element to append images
-
-// Array of image file names in the 'Source/Skills' directory
-for (let i = 1; i <= 10; i++) {
-  const img = new Image(); // Create a new image element
-  img.src = `Source/Skills/${i}.png`; // Set the image source dynamically
-  skillsContainer.appendChild(img); 
-}
